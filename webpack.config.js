33fded01bac9cd7f0094bc6f1660e3c7f5c87ae5
Vladislav <vlad.murashchenko95@gmail.com>
2018-04-17 18:20:03 +0300

    basic boilerplate with webpack, react, babel, babel-polyfill

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ["babel-polyfill", "./src"]
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ]
+};

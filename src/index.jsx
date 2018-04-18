8f5f893a1dad2a20a0d7947f9440528f05c99a94
Vladislav <vlad.murashchenko95@gmail.com>
2018-04-18 13:08:36 +0300

    basic template

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import reducer from './reducers';
+import ExamplePage from "./pages/ExamplePage"
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__());
+
 
 ReactDOM.render(
-  <div>
-    <p>React here!</p>
-  </div>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={ExamplePage} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
---
+++ src/pages/ExamplePage/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ExamplePage() {
+  return (
+    <div>
+      example
+    </div>
+  );
+}

5ac17c2998a02330c109cc248ea1b545e99ce100
Vladislav <vlad.murashchenko95@gmail.com>
2018-04-18 15:20:51 +0300

    add images resolve

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,18 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [

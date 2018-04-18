0d7614e0bbe915983c48f568887c8c5462671c4c
Vladislav <vlad.murashchenko95@gmail.com>
2018-04-18 10:40:21 +0300

    App.jsx => index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,9 @@
-import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <p>React here!</p>
+  </div>,
+  document.getElementById("root")
+);
